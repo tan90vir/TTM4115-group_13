a488712a34c8274222b44335d953e9c2bb3de7e6
antiloplastico <albertorob98@gmail.com>
2023-04-24 16:18:13 +0200

    updated bash

--- rawRATs/rat_downloader.sh
+++ rawRATs/rat_downloader.sh
@@ -4,5 +4,5 @@
 #url=${url//'/edit?usp=sharing'/}
 #url=$url"/gviz/tq?tqx=out:csv&sheet=$1"
 url="https://docs.google.com/spreadsheets/d/19LL73LN_uUWXpZN12LcX6Yikn6XPKuYvcBzpYfuB80I/gviz/tq?tqx=out:csv&sheet=$1"
-nRat=$(($(cat "$file" | wc -l) -1))
-wget "$url" -O "$1".csv
+wget "$url" -O "./RATs/$1".csv
+python3 ratToFirebase.py "$1"
